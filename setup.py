d243ee5eae54926ac3930a9132d59fe576c22afc
Lingheng Meng <linghengmeng@yahoo.com>
2018-04-22 12:41:17 -0400

    t

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 from setuptools import setup
 import os.path
 
-def gen_data_files(*dirs):
-    results = []
-
-    for src_dir in dirs:
-        for root,dirs,files in os.walk(src_dir):
-            results.append((root, map(lambda f:root + "/" + f, files)))
-    return results
-
 setup(
     name='vreppy',
     packages=['vreppy', 'vreppy.vrep'],
