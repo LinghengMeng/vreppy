e0eb0d4a808903097e066ba3276af1fe937e5c0b
Lingheng Meng <linghengmeng@yahoo.com>
2018-04-21 21:21:09 -0400

    t

--- setup.py
+++ setup.py
@@ -30,5 +30,5 @@ setup(
     data_files=[('lib/python3.5/site-packages/vreppy/scenes/', ['scenes/testAllComponents.ttt',
                                                                 'scenes/Pioneer.ttt',
                                                                 'scenes/LineFollowerPioneer.ttt']),
-                ('lib/python3.5/site-packages/vreppy/vrep/lib/Mac/',['lib/remoteApi.dylib'])]
+                ('lib/python3.5/site-packages/vreppy/lib/',['lib/remoteApi.dylib'])]
 )
