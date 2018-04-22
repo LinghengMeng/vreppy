dd3dba8fd6a37ee757a745a66c3371b108d219cc
Lingheng Meng <linghengmeng@yahoo.com>
2018-04-22 12:32:57 -0400

    t

--- examples/testallcomponent.py
+++ examples/testallcomponent.py
@@ -1,5 +1,5 @@
 import time
-from pyrep import VRep
+from vreppy import VRep
 from math import *
 # contextlib
 # simpy
@@ -16,22 +16,27 @@ with VRep.connect("127.0.0.1", 19997) as vrep:
     j_pas = vrep.joint.passive("joint_passive")
     j_sph = vrep.joint.spherical("sp_joint")
     j_spr = vrep.joint.spring("joint_spring")
+    prox_sensor = vrep.sensor.proximity("Proximity_sensor")
 
     s = vrep.sensor.proximity("sensor")
     v = vrep.sensor.vision("vision")
 
-    j_vel.set_target_velocity(2)
+    j_vel.set_target_velocity(1)
     j_spr.set_target_position(2)
     
+    print("Control joint_position...")
     for i in range(5):
         b = pi / 9
         j_pos.set_target_position(b * i + 0.2)
         time.sleep(1)
+        
+    print("Control joint_passive...")
     for i in range(50):
         v = sin(i / 10)
         j_pas.set_position(v)
         time.sleep(0.1)
 
+    print("Control sp_joint...")
     for i in range(1000):
         v = sin(i / 100) * (i / 1000)
         j_sph.set_matrix(
@@ -39,7 +44,15 @@ with VRep.connect("127.0.0.1", 19997) as vrep:
              0, 0, 0, 0,
              v, 0, 0, 0])
         time.sleep(0.01)
-
+    
+    print("Read Proximity Sensor...")
+    while True:
+        state, position = prox_sensor.read()
+        if position.get_x() != 0:
+            print("Detect something!")
+        else:
+            print("Nothing!")
+        #print("position:{}".format(position.distance()))
 
 #vrep.simulation.stop()
 
