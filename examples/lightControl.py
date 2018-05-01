b27ff3958f211afef0073c864cfffca4c54caf89
Lingheng Meng <linghengmeng@yahoo.com>
2018-05-01 16:31:32 -0400

    t

--- examples/lightControl.py
+++ examples/lightControl.py
@@ -21,6 +21,8 @@ with VRep.connect("127.0.0.1", 19997) as vrep:
     
     for i in range(10):
         Omnidirectional_light.set_light_state(i%2)
+        state, diffsePart, specularPart = Omnidirectional_light.get_light_state_and_color()
+        print("light state: {}".format(state))
         time.sleep(1)
         
     
