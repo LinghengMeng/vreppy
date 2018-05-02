dfa3b1f3ade49f4eba9bd5c2d60880996a3e38c0
Lingheng Meng <linghengmeng@yahoo.com>
2018-05-01 22:57:30 -0400

    t

--- vreppy/lights.py
+++ vreppy/lights.py
@@ -31,10 +31,9 @@ class AnyLight:
                                                                        'setLightStateAndColor',
                                                                        [self._handle, state],[],[],emptyBuff,
                                                                        self._def_op_mode)
-        if res==v.simx_return_ok:
-            print ('setLightStateAndColor works! ',retStrings[0]) # display the reply from V-REP (in this case, just a string)
-        else:
-            print ('Remote function call failed')
+        if res != v.simx_return_ok:
+            RemoteFunctionCallError(self._name)
+            
     
 #    def set_lihgt_color(self, target):
 #        # to do
@@ -49,7 +48,7 @@ class AnyLight:
                                                                                [self._handle],[],[],emptyBuff,
                                                                                v.simx_opmode_blocking)
         if res==v.simx_return_ok:
-            print ('getLightStateAndColor works! ',retStrings[0]) # display the reply from V-REP (in this case, just a string)
+            #print ('getLightStateAndColor works! ',retStrings[0]) # display the reply from V-REP (in this case, just a string)
             lightState = retInts[0]
             diffusePart = [retFloats[0],retFloats[1],retFloats[2]]
             specularPart = retFloats[3],retFloats[4],retFloats[5]
